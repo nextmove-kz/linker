eb6a5d6155827795d6e8effed108aa72fabd53d0
lukivan8 <lukivan888@gmail.com>
2025-01-08 16:10:53 +0500

    capitalize file

---
+++ src/components/formFields/AddressField.tsx
@@ -0,0 +1,18 @@
+import { Input } from "../ui/input";
+import FormField from "./FormField";
+
+type AddressFieldProps = {
+  name?: string;
+};
+
+export default function AddressField({ name = "Адрес" }: AddressFieldProps) {
+  return (
+    <FormField name={name}>
+      <Input name={name + "_street"} placeholder="Улица" />
+      <div className="flex gap-2">
+        <Input name={name + "_house_number"} placeholder="Дом" />
+        <Input name={name + "_apartment"} placeholder="Квартира" />
+      </div>
+    </FormField>
+  );
+}
