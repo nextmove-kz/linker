23b5398a0ed8076172ae58365511dd78efaf5751
anu-mdl <sassuwunnuqq@gmail.com>
2025-01-13 16:41:56 +0500

    message refactoring

--- src/api/order.ts
+++ src/api/order.ts
@@ -1,16 +1,17 @@
 "use server";
 
 import { pocketbase } from "./pocketbase";
+import { OrdersRecord } from './api_types';
 
 
 export async function getOrder(id: string) {
     try {
         const pb = await pocketbase();
         const order = await pb.collection("orders").getOne(id);
-        return order;
+        return order as OrdersRecord;
     } catch (error) {
       console.error("Order error:", error);
-      return { error: "Failed to get order" };
+      return null;
     }
 }
 
--- src/app/message/[id]/page.tsx
+++ src/app/message/[id]/page.tsx
@@ -1,62 +1,81 @@
 import { OrdersRecord } from "@/api/api_types";
 import { getOrder } from "@/api/order";
+import { OrdersResponse } from "../../../api/api_types";
 
 // пУЬ К фАаЙЛу
 const Message = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const order = await getOrder(id);
+  if (!order) return;
 
-  // TODO: возможны доработки
-  function createMessage(order: Object) {
-    let message = "";
-    const addressParts = [];
-    const selectParts = [];
-    const datetimeParts = [];
-    const groupParts = [];
-    const countParts = [];
-    const multichoiceParts = [];
-    const textParts = [];
-    const textareaParts = [];
-    const phoneParts = [];
+  const proceessAddress = (orderData: Object, keys: string[], name: string) => {
+    const streetKey = `${name}_street`;
+    const housenumberKey = `${name}_housenumber`;
+    const apartmentKey = `${name}_apartment`;
+    const streetValue = orderData[streetKey as keyof typeof orderData];
+    const housenumberValue =
+      orderData[housenumberKey as keyof typeof orderData];
+    const apartmentValue = orderData[apartmentKey as keyof typeof orderData];
 
-    for (const [key, value] of Object.entries(order)) {
-      if (
-        key.endsWith("_apartment") ||
-        key.endsWith("_housenumber") ||
-        key.endsWith("_street")
-      ) {
-        addressParts.push(value);
-      } else if (key.endsWith("_select")) {
-        message += `${key.split("_")[0]}: ${value}\n`;
-      } else if (key.endsWith("_datetime")) {
-        message += `${key.split("_")[0]}: ${value}\n`;
-      } else if (key.endsWith("_group")) {
-        message += `${key.split("_")[0]}: ${value}\n`;
-      } else if (key.endsWith("_count")) {
-        message += `${key.split("_")[0]}: ${value}\n`;
-      } else if (key.endsWith("_multichoice")) {
-        message += `${key.split("_")[0]}: ${value.join(", ")}\n`;
-      } else if (key.endsWith("_text")) {
-        message += `${key.split("_")[0]}: ${value}\n`;
-      } else if (key.endsWith("_textarea")) {
-        message += `${key.split("_")[0]}: ${value}\n`;
-      } else if (key.endsWith("_phone")) {
-        message += `${key.split("_")[0]}: ${value}\n`;
+    const addressString = `${name}: ул. ${streetValue} дом ${housenumberValue} кв. ${apartmentValue}\n`;
+
+    keys.splice(keys.indexOf(streetKey), 1);
+    keys.splice(keys.indexOf(housenumberKey), 1);
+    keys.splice(keys.indexOf(apartmentKey), 1);
+
+    return addressString;
+  };
+
+  const compileMessage = (orderData: Object) => {
+    const complexTypes = [
+      "apartment",
+      "housenumber",
+      "street",
+      "multichoice",
+      "files",
+    ];
+    const keys = Object.keys(orderData);
+
+    let resultString = "";
+
+    for (const key of keys) {
+      const name = key.split("_")[0];
+      const type = key.split("_")[1];
+      const value: any = orderData[key as keyof typeof orderData];
+
+      // console.log("resultString", resultString);
+
+      let dataString;
+      console.log("type", type);
+      if (complexTypes.includes(type)) {
+        console.log("is_complex");
+        if (
+          type === "apartment" ||
+          type === "housenumber" ||
+          type === "street"
+        ) {
+          dataString = proceessAddress(orderData, keys, name);
+        }
+        if (type === "multichoice") {
+          dataString = `${name}: ${value.join(", ")}\n`;
+        }
+        if (type === "files") {
+          dataString = "";
+        }
+      } else {
+        dataString = `${name}: ${value}\n`;
       }
+
+      resultString += dataString;
     }
+    return resultString;
+  };
 
-    if (addressParts.length > 0) {
-      message += `Адрес: ${addressParts.join(", ")}\n`;
-    }
-
-    return message;
-  }
-
-  const message = createMessage(order.orderData);
+  const message = compileMessage(order.orderData as OrdersRecord);
   return (
     <div className="flex flex-col items-center justify-center">
       <p>IAESFHOIEvfovojewvik</p>
-      <p>{message}</p>
+      <pre>{message}</pre>
     </div>
   );
 };
