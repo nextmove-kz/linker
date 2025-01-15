3e3e1ca79bc13274053d3babe298d2723935bcd9
anu-mdl <sassuwunnuqq@gmail.com>
2025-01-15 10:31:38 +0500

    payment hot-fix

--- src/app/[id]/payment/page.tsx
+++ src/app/[id]/payment/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 
 import Branding from "@/components/branding";
-import FormField from "@/components/formFields/FormField";
 import { InputField } from "@/components/formFields/FormInput";
 import PhoneField from "@/components/formFields/phone/PhoneField";
 import PaymentCard from "@/components/payment/PaymentCard";
 import { Button } from "@/components/ui/button";
 import {
-  CreditCard,
-  Gift,
-  BanknoteIcon as Bank,
   CircleDollarSignIcon as Money,
   ReceiptText as Bill,
   ArrowLeftRight as Transfer,
 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { on } from "events";
 
 const paymentMethods = [
   { id: "kaspi-pt", name: "Каспи платеж", icon: Bill },
@@ -24,6 +21,25 @@ const paymentMethods = [
 
 const PaymentPage = () => {
   const [selectedMethod, setSelectedMethod] = useState<string | null>(null);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    let data;
+    if (selectedMethod === "kaspi-pt") {
+      data = {
+        phoneNumber: formData.get("Ваш номер для платежа_phone") || "",
+      };
+    } else if (selectedMethod === "cash") {
+      data = {
+        amount: formData.get("Сдача с какой суммы?") || "",
+      };
+    }
+
+    console.log(data);
+  };
+
   return (
     <div className="flex flex-col gap-4 max-w-[400px] p-2 mx-auto">
       <Branding />
@@ -42,25 +58,31 @@ const PaymentPage = () => {
       <div className="pt-2">
         {selectedMethod === "kaspi-pt" && (
           <form
-            action=""
-            className="flex flex-col p-2 w-max-[400px] gap-4 mx-auto"
+            onSubmit={onSubmit}
+            className="flex flex-col p-2 max-w-[400px] gap-4 mx-auto"
           >
             <PhoneField name="Ваш номер для платежа" />
+            <Button type="submit" className="mt-2">
+              Продолжить
+            </Button>
           </form>
         )}
         {selectedMethod === "cash" && (
           <form
-            action=""
-            className="flex flex-col p-2 w-max-[400px] gap-4 mx-auto"
+            onSubmit={onSubmit}
+            className="flex flex-col p-2 max-w-[400px] gap-4 mx-auto"
           >
             <InputField name="Сдача с какой суммы?" placeholder="100" />
+            <Button type="submit" className="mt-2">
+              Продолжить
+            </Button>
           </form>
         )}
         {selectedMethod === "kaspi-transfer" && (
-          <div className="flex flex-col p-2 w-max-[400px] gap-4 mx-auto text-sm">
+          <div className="flex flex-col p-2 max-w-[400px] gap-4 mx-auto text-sm">
             <span>Каспи перевод</span>
             <span>Номер для перевода: +7 (890) 123-45-67</span>
-            <Button />
+            <Button className="mt-2">Продолжить</Button>
           </div>
         )}
       </div>
