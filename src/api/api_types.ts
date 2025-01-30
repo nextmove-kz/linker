d646b842262db95c24b283ca7a01fb246aa6eec7
anu-mdl <sassuwunnuqq@gmail.com>
2025-01-30 13:14:23 +0500

    typegen

--- src/api/api_types.ts
+++ src/api/api_types.ts
@@ -26,7 +26,6 @@ export enum Collections {
 	Settings = "settings",
 	ShoppingCart = "shopping_cart",
 	Subscribers = "subscribers",
-	Users = "users",
 }
 
 // Alias types for improved usability
@@ -101,12 +100,21 @@ export type SuperusersRecord = {
 }
 
 export type BusinessRecord = {
+	address?: string
+	color?: string
 	created?: IsoDateString
-	displayName?: string
+	description?: string
+	display_name?: string
+	email: string
+	emailVisibility?: boolean
 	id: string
-	name: string
-	phone_number: string
+	instagram?: string
+	name?: string
+	password: string
+	phone_number?: string
+	tokenKey: string
 	updated?: IsoDateString
+	verified?: boolean
 }
 
 export type BusinessFieldSettingsRecord = {
@@ -119,7 +127,7 @@ export type BusinessFieldSettingsRecord = {
 }
 
 export type BusinessFieldsRecord = {
-	business: RecordIdString
+	business?: RecordIdString
 	created?: IsoDateString
 	field_options?: RecordIdString[]
 	field_settings?: RecordIdString[]
@@ -133,7 +141,7 @@ export type BusinessFieldsRecord = {
 
 export type DetailsRecord<TorderData = unknown> = {
 	attachments?: string[]
-	business: RecordIdString
+	business?: RecordIdString
 	created?: IsoDateString
 	device_id: string
 	id: string
@@ -200,6 +208,7 @@ export type OrdersRecord<Tpayment = unknown> = {
 	id: string
 	items: RecordIdString[]
 	payment?: null | Tpayment
+	phone?: string
 	status?: OrdersStatusOptions
 	updated?: IsoDateString
 }
@@ -267,26 +276,13 @@ export type SubscribersRecord = {
 	updated?: IsoDateString
 }
 
-export type UsersRecord = {
-	avatar?: string
-	created?: IsoDateString
-	email: string
-	emailVisibility?: boolean
-	id: string
-	name?: string
-	password: string
-	tokenKey: string
-	updated?: IsoDateString
-	verified?: boolean
-}
-
 // Response types include system fields and match responses from the PocketBase API
 export type AuthoriginsResponse<Texpand = unknown> = Required<AuthoriginsRecord> & BaseSystemFields<Texpand>
 export type ExternalauthsResponse<Texpand = unknown> = Required<ExternalauthsRecord> & BaseSystemFields<Texpand>
 export type MfasResponse<Texpand = unknown> = Required<MfasRecord> & BaseSystemFields<Texpand>
 export type OtpsResponse<Texpand = unknown> = Required<OtpsRecord> & BaseSystemFields<Texpand>
 export type SuperusersResponse<Texpand = unknown> = Required<SuperusersRecord> & AuthSystemFields<Texpand>
-export type BusinessResponse<Texpand = unknown> = Required<BusinessRecord> & BaseSystemFields<Texpand>
+export type BusinessResponse<Texpand = unknown> = Required<BusinessRecord> & AuthSystemFields<Texpand>
 export type BusinessFieldSettingsResponse<Texpand = unknown> = Required<BusinessFieldSettingsRecord> & BaseSystemFields<Texpand>
 export type BusinessFieldsResponse<Texpand = unknown> = Required<BusinessFieldsRecord> & BaseSystemFields<Texpand>
 export type DetailsResponse<TorderData = unknown, Texpand = unknown> = Required<DetailsRecord<TorderData>> & BaseSystemFields<Texpand>
@@ -301,7 +297,6 @@ export type SettingVariantResponse<Texpand = unknown> = Required<SettingVariantR
 export type SettingsResponse<Texpand = unknown> = Required<SettingsRecord> & BaseSystemFields<Texpand>
 export type ShoppingCartResponse<Texpand = unknown> = Required<ShoppingCartRecord> & BaseSystemFields<Texpand>
 export type SubscribersResponse<Texpand = unknown> = Required<SubscribersRecord> & BaseSystemFields<Texpand>
-export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
 
 // Types containing all Records and Responses, useful for creating typing helper functions
 
@@ -326,7 +321,6 @@ export type CollectionRecords = {
 	settings: SettingsRecord
 	shopping_cart: ShoppingCartRecord
 	subscribers: SubscribersRecord
-	users: UsersRecord
 }
 
 export type CollectionResponses = {
@@ -350,7 +344,6 @@ export type CollectionResponses = {
 	settings: SettingsResponse
 	shopping_cart: ShoppingCartResponse
 	subscribers: SubscribersResponse
-	users: UsersResponse
 }
 
 // Type for usage with type asserted PocketBase instance
@@ -377,5 +370,4 @@ export type TypedPocketBase = PocketBase & {
 	collection(idOrName: 'settings'): RecordService<SettingsResponse>
 	collection(idOrName: 'shopping_cart'): RecordService<ShoppingCartResponse>
 	collection(idOrName: 'subscribers'): RecordService<SubscribersResponse>
-	collection(idOrName: 'users'): RecordService<UsersResponse>
 }
---
+++ src/pocketbase/pb_migrations/1738224543_deleted_business2.js
@@ -0,0 +1,101 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1218262561");
+
+  return app.delete(collection);
+}, (app) => {
+  const collection = new Collection({
+    "createRule": "",
+    "deleteRule": "",
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text1579384326",
+        "max": 21,
+        "min": 0,
+        "name": "name",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": false,
+        "required": true,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text3786665039",
+        "max": 21,
+        "min": 0,
+        "name": "displayName",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text1795275867",
+        "max": 0,
+        "min": 0,
+        "name": "phone_number",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": true,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "autodate2990389176",
+        "name": "created",
+        "onCreate": true,
+        "onUpdate": false,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "autodate3332085495",
+        "name": "updated",
+        "onCreate": true,
+        "onUpdate": true,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      }
+    ],
+    "id": "pbc_1218262561",
+    "indexes": [
+      "CREATE UNIQUE INDEX `idx_PxcL4qa70H` ON `business2` (`name`)"
+    ],
+    "listRule": "",
+    "name": "business2",
+    "system": false,
+    "type": "base",
+    "updateRule": "",
+    "viewRule": ""
+  });
+
+  return app.save(collection);
+})
