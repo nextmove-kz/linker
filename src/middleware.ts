39ec0068fde91863473fe231dad2493f76f7b615
lukivan8 <lukivan888@gmail.com>
2025-02-10 13:23:14 +0500

    added handling for protected routes list

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,23 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 import { handleDeviceId } from "./middleware/deviceId";
 import { isLoggedIn } from "./api/auth/sign-in";
 
-export async function middleware(request: NextRequest) {
-  const auth = await isLoggedIn();
-  const path = request.nextUrl.pathname;
+const PROTECTED_ROUTES = ["/auth/sign-up/details/", "/order/"];
 
+export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
   let response = NextResponse.next();
 
   response = await handleDeviceId(request, response);
 
-  if (path !== "/" && !(path.includes("/auth"))) {
-    if (!auth) {
-      return NextResponse.redirect(new URL('/auth/sign-in', request.url));
+  // Если хоть один путь начинается с защищенного префикса, то надо проверить авторизацию
+  const requiresAuth = PROTECTED_ROUTES.some((route) => path.startsWith(route));
+
+  if (requiresAuth) {
+    const isAuthenticated = await isLoggedIn();
+
+    if (!isAuthenticated) {
+      return NextResponse.redirect(new URL("/auth/sign-in", request.url));
     }
   }
 
-  return response
+  return response;
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)']
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
